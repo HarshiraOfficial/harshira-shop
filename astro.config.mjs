e2984f0a584a913f2b6b8d5422328fe3ab81b24c
HarshiraOfficial <harshira253@gmail.com>
2026-03-08 00:08:06 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+// export default defineConfig({
+//   vite: {
+//     plugins: [tailwindcss()]
+//   }
+// });
+
 export default defineConfig({
+  // Modern standard: explicitly define your production URL
+  site: 'https://harshiraofficial.github.io',
   vite: {
     plugins: [tailwindcss()]
   }

cacac4c2ba69d2584e1b30fcf9c201b54b89dd37
HarshiraOfficial <harshira253@gmail.com>
2026-03-08 00:39:21 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'astro/config';
 
 /* import data from "./OpenLinks.json"; */
@@ -6,20 +5,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// export default defineConfig({
-//   vite: {
-//     plugins: [tailwindcss()]
-//   }
-// });
-
 export default defineConfig({
-  // Modern standard: explicitly define your production URL
-// This tells Astro where the site is hosted
-  site: 'https://harshiraofficial.github.io',
-  
-  // This tells Astro to prefix all assets with /harshira-shop/
-  base: '/harshira-shop',
-  
   vite: {
     plugins: [tailwindcss()]
   }
